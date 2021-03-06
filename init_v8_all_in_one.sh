3c646a80ce1c9b80f7a303315d2465133560c1e5
Wei Wu <lazyparser@gmail.com>
2021-03-06 09:32:29 +0800

    Improve init_v8_all_in_one.sh (#9)
    Add more notice and comments.
    Remove the build patch that no longer needed.

--- init_v8_all_in_one.sh
+++ init_v8_all_in_one.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Ask Questions here:
+#    https://github.com/v8-riscv/v8-riscv-tools
+
 # Note: This script is used for the V8 RISCV Developers.
 #       Users are expected to read and modify the script self.
 #       If you want to get you hands dirty on v8-riscv project,
 #       the better way is to follow the project's wiki page, step by step.
+
 
 set -e
 
@@ -15,63 +19,51 @@ set -e
 #            does undefined behaviors.
 V8_ROOT=$PWD/v8-riscv
 RV_HOME=$HOME/opt/riscv
+DEPOT_DIR=$PWD/depot_tools
 
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+[ -d "$DEPOT_DIR" ] || git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git "$DEPOT_DIR"
 
-export PATH="$PWD/depot_tools:$PATH"
+export PATH="$DEPOT_DIR:$PATH"
 
 # I suppose you are using bash and I suppose you'd like the export.
 # Comment the line below if you don't need it.
-echo "export PATH='$PWD/depot_tools:$PATH'" >> ~/.bashrc
 
-# just a check
-gclient
+echo "export PATH='$DEPOT_DIR:$PATH'" >> ~/.bashrc
 
 # ref: https://github.com/v8-riscv/v8/wiki/get-the-source
+
+# if $V8_PORT exists, mkdir is intend to fail the script.
 mkdir -p $V8_ROOT
 cd $V8_ROOT
 fetch v8
-cd v8
 
-# Deprecate: for the riscv-porting-dev branch. switched to 'riscv64'
-#            since Aug 13 2020.
-#git checkout 8.1.268
-
-# pwd: $V8_ROOT/v8
-# Deprecate: for the riscv-porting-dev branch. switched to 'riscv64'
-#            since Aug 13 2020.
-#pushd third_party/icu
-#git checkout dbd3825b31041d782c5b504c59dcfb5ac7dda08c
-#popd
-
-# cd $V8_ROOT/v8
-git remote add riscv https://github.com/v8-riscv/v8.git
-# or alternatively using ssh if you are developer
-#git remote add riscv git@github.com:v8-riscv/v8.git
+# Currently the RISC-V is a tier-3 backend of chromium/v8, which means
+# that new commits in upstream might break riscv64 backend without
+# notice. So we maintain a stable branch for RISC-V.
+cd $V8_ROOT/v8
+git remote | grep -q riscv || \
+	git remote add riscv https://github.com/v8-riscv/v8.git
+	# or alternatively using ssh if you are developer
+	#git remote add riscv git@github.com:v8-riscv/v8.git
 git fetch riscv
-# Deprecate: for the riscv-porting-dev branch. switched to 'riscv64'
-#            since Aug 13 2020.
-#git checkout riscv-porting-dev
-git checkout riscv64
-
-# the patches folder was removed before upstreaming.
-# And you can download it in another repo.
-wget -O build.patch https://raw.githubusercontent.com/v8-riscv/v8-riscv-tools/main/riscv64-cross-build.patch
-cd build
-git apply ../build.patch
-cd ..
-# Remember to use `git stash` before you run `gclient sync`
-
+git checkout riscv/riscv64
 
 # Install deps. may need sudo
 # tip: remember add `--no-chromeos-fonts` if you are in mainland China
-./build/install-build-deps.sh --no-chromeos-fonts
+echo "Now the source code is ready."
+echo "Next action is to install deps, which may need sudo permission."
+echo "Open a new terminal/window and enter:"
+echo
+echo "$V8_ROOT/v8/build/install-build-deps.sh --no-chromeos-fonts"
+echo
+echo "After you get the deps installation done, Press ENTER:"
+read -p "(or Ctrl-C to cancel this script:) >>>"
 
 ###########################################################
 # Simulator Build
 ###########################################################
 
-# cd $V8_ROOT/v8
+cd $V8_ROOT/v8
 gn gen out/riscv64.sim \
     --args='is_component_build=false
     is_debug=true target_cpu="x64"
@@ -88,9 +80,19 @@ ninja -C out/riscv64.sim -j $(nproc)
 # Cross Compile to RISC-V64
 ###########################################################
 
+echo "Congrats! Now you just have built a V8 for RISC-V (simulator mode)!"
+echo "Everything looks good!"
+echo "Now we are diving into cross compilation and testing, which could be a bit painful."
+echo "Feel free to stop here. Press ENTER to continue, Crtl-C to cancel:"
+read -p ">>>"
+
 # +++++++++++++++++++++++++++++++++++++++++
 # RISC-V GNU Toolchain
 # +++++++++++++++++++++++++++++++++++++++++
+
+echo "Ubuntu 18.04 does not have the latest riscv64 toolchain in place."
+echo "It has gcc-7 and gcc-8, while the latest is gcc-10.2"
+echo "We are going to build a latest toolchain for cross building."
 
 mkdir -p $RV_HOME
 
